56ca25ffecbaf962be975982b2c3b6e609a5794a
Fazliddin <fazikxolmatov-dot@gmail.com>
2026-03-10 13:49:11 +0000

    days

---
+++ salom.sh
@@ -0,0 +1,7 @@
+!/bin/bash
+
+# Muallif: DevOps Mentor
+# Vazifa: Skript vazifasini tushuntirish
+# Bu qator skriptning nima qilishini tushuntiradi
+echo "Izohlar skript ishlashiga ta'sir qilmaydi!"
+
---
+++ salom2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ISMI="Ali"
+YOSHI=25
+echo "Mening ismim $ISMI va yoshim $YOSHI da."
+
